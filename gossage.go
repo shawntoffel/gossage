5924e9a92746657c5908eae6fe8325ffd5262efb
Shawn Toffel <shawn.toffel@gmail.com>
2019-01-13 20:23:45 +0000

    Added ability to register migrations

--- gossage.go
+++ gossage.go
@@ -1,6 +1,27 @@
 package gossage
 
-type Gossage struct{}
+import "fmt"
+
+type Gossage struct {
+	migrations map[string]Migration
+}
+
+func (g Gossage) RegisterMigration(m Migration) error {
+	if g.migrations == nil {
+		g.migrations = map[string]Migration{}
+	}
+
+	version := m.Version()
+
+	_, exists := g.migrations[version]
+	if exists {
+		return fmt.Errorf("gossage: a migration with version '%s' has already been registered.", version)
+	}
+
+	g.migrations[version] = m
+
+	return nil
+}
 
 func (g Gossage) Up() error {
 
--- migration.go
+++ migration.go
@@ -3,6 +3,7 @@ package gossage
 import "database/sql"
 
 type Migration interface {
+	Version() string
 	Up(*sql.Tx) error
 	Down(*sql.Tx) error
 }
